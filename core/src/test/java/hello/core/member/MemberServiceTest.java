d4a89fb9a661196dd1b4c4722839bb3e00c5fc0f
jhlee0413 <edcdia2@kakao.com>
2022-08-19 18:24:58 +0900

    day3

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    private MemberRepository memberRepository;
     MemberService memberService = new MemberServiceImpl(memberRepository);
 
     @Test
