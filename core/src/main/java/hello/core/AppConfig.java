1541c1f3a54b720192a6a660c979d7f73e235185
jhlee0413 <edcdia2@kakao.com>
2022-09-19 21:25:16 +0900

    의존관계 주입

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,23 +15,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository() 호출
+    //@Bean orderService -> new MemoryMemberRepository() 호출
+    //위 상황에서 싱글톤이 깨질까?
+
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
+        System.out.println("call AppConfig.discountPolicy");
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
 
